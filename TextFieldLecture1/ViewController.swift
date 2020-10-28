5f20fa9d0f09d2918ec0f4d27a633f79fab03766
Miguel Aenlle <maenlle4004@stu.d214.org>
2020-10-28 12:27:31 -0700

    Initial Commit

---
+++ TextFieldLecture1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextFieldLecture1
+//
+//  Created by user172344 on 10/28/20.
+//  Copyright © 2020 user172344. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
